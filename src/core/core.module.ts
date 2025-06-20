accc36f5c47879ea084953dad2a9043d0602d780
Sobirjon <ahmadjonovsobirjon24@gmail.com>
2025-06-20 18:25:16 +0500

    mm

---
+++ src/core/core.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      global: true,
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_KEY') as string,
+        signOptions: {
+          expiresIn: '1d',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [],
+})
+export class CoreModule {}
